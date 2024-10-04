24677945ca09290e228b0b54b5260213059aee74
lucas <lucas.moreau@supinfo.com>
2024-10-04 11:41:03 +0200

    fix style bug iin main page

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -19,15 +19,17 @@
 
 
 <div class="min-h-screen bg-background flex flex-col items-center justify-center p-4">
-    <main class="max-w-lg w-full space-y-4">
+    <main class="w-full">
         <h1 class="text-3xl font-bold text-center text-foreground">SCD-17</h1>
-        <div class="space-y-4">
+        <div class="space-y-4 mx-auto text-center pt-4" style="max-width: 600px;">
             <Button class="w-full" size="lg" href="/clients">
                 <PlusCircle class="mr-2 h-5 w-5" />
                 Ajouter un client
             </Button>
+        </div>
+        <div class="space-y-6">
             {#if clients.length !== 0}
-                <div class="w-full overflow-x-auto">
+                <div class="w-full overflow-x-auto mx-auto" style="max-width: 600px;">
                     <DataTable clients={clients}/>
                 </div>
             {:else}
@@ -36,3 +38,6 @@
         </div>
     </main>
 </div>
+
+
+
